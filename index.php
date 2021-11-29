61fcc02ef118ddd2a72d32d956e5eb405164484d
tibaa003 <91213295+tibaa003@users.noreply.github.com>
2021-11-29 09:32:22 +0100

    origin

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="./login.php">login</a>
+    </header>
+    <div class="contentContainer">
+        <div class="imageContainer">
+            <img src="./assets/ev1.jpg" alt="ev1">
+        </div>
+        <div class="descriptionContainer">
+            <h1>even quiz</h1>
+        </div>
+    </div>
+    <div class="contentContainer">
+        <div class="descriptionContainer">
+            <h1>gamin</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="" alt="">
+        </div>
+    </div>
+</body>
+</html>
