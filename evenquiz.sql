2d87f7be63712d855c5b962b1e89397f5e61229e
tibaa003 <91213295+tibaa003@users.noreply.github.com>
2022-04-24 14:45:58 +0200

    added sql database file
    xml files cringe appearently

---
+++ evenquiz.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2022 at 02:45 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `evenquiz`
+--
+CREATE DATABASE IF NOT EXISTS `evenquiz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `evenquiz`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evenquizquestions`
+--
+
+CREATE TABLE `evenquizquestions` (
+  `ID` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `answer` varchar(255) NOT NULL,
+  `option1` varchar(255) DEFAULT NULL,
+  `option2` varchar(255) DEFAULT NULL,
+  `option3` varchar(255) DEFAULT NULL,
+  `imgName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evenquizstats`
+--
+
+CREATE TABLE `evenquizstats` (
+  `userID` int(11) NOT NULL,
+  `question` int(11) NOT NULL,
+  `correctAnswers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faqs`
+--
+
+CREATE TABLE `faqs` (
+  `ID` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `answer` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `polishcowmilkerstats`
+--
+
+CREATE TABLE `polishcowmilkerstats` (
+  `userID` int(11) NOT NULL,
+  `milk` int(11) NOT NULL,
+  `milkers` int(11) NOT NULL,
+  `slaves` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` char(97) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `evenquizquestions`
+--
+ALTER TABLE `evenquizquestions`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `evenquizstats`
+--
+ALTER TABLE `evenquizstats`
+  ADD PRIMARY KEY (`userID`),
+  ADD KEY `question` (`question`);
+
+--
+-- Indexes for table `faqs`
+--
+ALTER TABLE `faqs`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `polishcowmilkerstats`
+--
+ALTER TABLE `polishcowmilkerstats`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `evenquizquestions`
+--
+ALTER TABLE `evenquizquestions`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `evenquizstats`
+--
+ALTER TABLE `evenquizstats`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `faqs`
+--
+ALTER TABLE `faqs`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `polishcowmilkerstats`
+--
+ALTER TABLE `polishcowmilkerstats`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `evenquizstats`
+--
+ALTER TABLE `evenquizstats`
+  ADD CONSTRAINT `evenquizstats_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `evenquizstats_ibfk_2` FOREIGN KEY (`question`) REFERENCES `evenquizquestions` (`ID`);
+
+--
+-- Constraints for table `polishcowmilkerstats`
+--
+ALTER TABLE `polishcowmilkerstats`
+  ADD CONSTRAINT `polishcowmilkerstats_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
