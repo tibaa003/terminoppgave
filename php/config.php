e0dcec810458ffd314c81e4a664595523ba47832
Titas <tibaa003@osloskolen.no>
2022-05-27 14:49:34 +0200

    Hidden config credentials
    Config changes no longer getting pushed only saved locally for security reasons

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials.*/
-define('DB_SERVER', 'localhost'); // 192.168.1.234:3306
-define('DB_USERNAME', 'root');  // 3s33t
-define('DB_PASSWORD', ''); // o78V4T835KU6
-define('DB_NAME', 'evenquiz');
+define('DB_SERVER', ''); // Database ip-adress
+define('DB_USERNAME', '');  // Database username
+define('DB_PASSWORD', ''); // Database password
+define('DB_NAME', ''); // Database name
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
